d0158417324b19fcb14a6f5315c52146468ced9b
Rohit <rohitrkd4997@gmail.com>
2021-10-17 13:46:41 +0530

    Basic structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './MyComponents/navbar/navbar.component';
+import { FooterComponent } from './MyComponents/footer/footer.component';
+import { BodyComponent } from './MyComponents/body/body.component';
+import { IntroComponent } from './MyComponents/intro/intro.component';
+import { AboutmeComponent } from './MyComponents/aboutme/aboutme.component';
+import { ProjectsComponent } from './MyComponents/projects/projects.component';
+import { SkillsComponent } from './MyComponents/skills/skills.component';
+import { ContactMeComponent } from './MyComponents/contact-me/contact-me.component';
+import { SideBarComponent } from './MyComponents/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    BodyComponent,
+    IntroComponent,
+    AboutmeComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactMeComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
